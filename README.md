0ce60ea08a724d9748882445b56c787da6c86948
Clauz Fang <clauzjdz@gmail.com>
2017-06-21 22:02:03 +0800

    Adapt standard js avoid bikeshedding

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sliding Box
 
-[![Build Status](https://travis-ci.org/idingding/sliding-box.svg?branch=master)](https://travis-ci.org/idingding/sliding-box)
+[![Build Status](https://travis-ci.org/idingding/sliding-box.svg?branch=master)](https://travis-ci.org/idingding/sliding-box) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
 Aim to offer a better web app for users to check when the next trams will come.
 Intend to be fast, bandwidth-saving, mobile-first and user-friendly.
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
+const path = require('path')
+
 module.exports = {
-    entry: './src/scripts/app.js',
-    output: {
-        path: __dirname + '/build',
-        filename: 'app.min.js'
-    },
-    resolve: {
-        alias: {
-            'vue': 'vue/dist/vue.min.js'
-        }
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['es2015', { modules: false }]
-                    ]
-                }
-            },
-            {
-                test: /\.scss$/,
-                loaders: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
+  entry: './src/scripts/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.min.js'
+  },
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.min.js'
     }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+                        ['es2015', { modules: false }]
+          ]
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
 }
